00e5b114711837e057bff9959844b4acb3b0d338
JJPeng <pengjiajun755@163.com>
2022-07-23 22:54:56 +0800

    使用密码授予类型-配置注册的客户端 测试：因为是客户端代表用户去请求授权服务器获取授权的，所以客户端需要先用自己的clientId和secret标识出自己的身份（基于http basic认证），并携带用户给客户端提供的用户名和用户密码作为参数 curl -v -XPOST -u client:secret "http://localhost:8080/oauth/token?grant_type=password&username=john&password=12345&scope=read" 返回：{"access_token":"c8769d80-7897-4bca-946e-fe585d75f8ee","token_type":"bearer","expires_in":43199,"scope":"read"} 在spring security的默认配置中令牌就只是一个简单的uuid

--- src/main/java/com/jjpeng/authorizationserver/config/AuthConfiguration.java
+++ src/main/java/com/jjpeng/authorizationserver/config/AuthConfiguration.java
@@ -3,6 +3,7 @@ package com.jjpeng.authorizationserver.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
@@ -23,4 +24,36 @@ public class AuthConfiguration extends AuthorizationServerConfigurerAdapter {
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager);
     }
+
+
+    //详细版本的配置，当使用数据库存储client details是应该使用详细的配置
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        InMemoryClientDetailsService inMemoryClientDetailsService = new InMemoryClientDetailsService();
+//
+//        BaseClientDetails clientDetails = new BaseClientDetails();
+//        clientDetails.setClientId("client");
+//        clientDetails.setClientSecret("secret");
+//        clientDetails.setScope(Arrays.asList("read"));
+//        clientDetails.setAuthorizedGrantTypes(Arrays.asList("password"));
+//
+//        HashMap<String, ClientDetails> map = new HashMap<>();
+//        //key:value -> clientId:clientDetails
+//        map.put("client", clientDetails);
+//        inMemoryClientDetailsService.setClientDetailsStore(map);
+//
+//        clients.withClientDetails(inMemoryClientDetailsService);
+//    }
+
+
+    //简短版本的配置
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("client")
+                .secret("secret")
+                .authorizedGrantTypes("password")
+                .scopes("read");
+    }
+
 }
