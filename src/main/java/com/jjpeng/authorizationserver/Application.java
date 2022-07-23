9ddebb2d4d3e14c7941701be837912d03b783ecc
JJPeng <pengjiajun755@163.com>
2022-07-23 20:55:52 +0800

    添加OAuth2授权服务器所需依赖，创建主启动类

---
+++ src/main/java/com/jjpeng/authorizationserver/Application.java
@@ -0,0 +1,15 @@
+package com.jjpeng.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author JJPeng
+ * @date 2022/7/23 20:53
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
