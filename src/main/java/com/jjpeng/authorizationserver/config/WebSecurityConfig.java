b26ff555906326428422d71412d46c270adff057
JJPeng <pengjiajun755@163.com>
2022-07-23 21:41:34 +0800

    添加用户管理的配置，使用InMemoryUserDetailsManager作为UserDetailsService,并用NoOpPasswordEncoder作为密码编码器

---
+++ src/main/java/com/jjpeng/authorizationserver/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.jjpeng.authorizationserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+/**
+ * @author JJPeng
+ * @date 2022/7/23 21:16
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+        inMemoryUserDetailsManager.createUser(user);
+        return inMemoryUserDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
