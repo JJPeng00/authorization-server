af9a6091bbcf0c071f221da6158c5f711d9a0295
JJPeng <pengjiajun755@163.com>
2022-07-25 22:48:24 +0800

    配置资源服务器与授权服务器共享数据库以保存令牌， 授权服务器授权后将令牌保存进数据库中 资源服务器需要验证令牌时，去数据库中查询，而不是请求授权服务器验证。 测试生成token： curl -v -XPOST -u client:secret "http://localhost:8080/oauth/token?grant_type=password&username=john&password=12345&scope=read" 结果： {"access_token":"1bf8019a-d9de-444b-9aeb-92b7d1db665f","token_type":"bearer","refresh_token":"708caf94-43fd-405e-96aa-cb37fa100f8c","expires_in":42597,"scope":"read"}

--- src/main/java/com/jjpeng/authorizationserver/config/AuthConfiguration.java
+++ src/main/java/com/jjpeng/authorizationserver/config/AuthConfiguration.java
@@ -1,12 +1,18 @@
 package com.jjpeng.authorizationserver.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
+
 
 /**
  * @author JJPeng
@@ -19,10 +25,18 @@ public class AuthConfiguration extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.authenticationManager(authenticationManager)
+                .tokenStore(tokenStore());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
     }
 
 
