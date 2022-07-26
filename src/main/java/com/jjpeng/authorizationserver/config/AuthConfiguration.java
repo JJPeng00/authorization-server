0b536b15d54ae68ef0c31bc565fc40afdc948fbf
JJPeng <pengjiajun755@163.com>
2022-07-26 23:20:10 +0800

    在jwt中增加自定义信息 测试生成token： curl -v -XPOST -u client:secret "http://localhost:8080/oauth/token?grant_type=password&username=john&password=12345&scope=read" 结果： {"access_token":"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJnZW5lcmF0ZUluWm9uZSI6IkFzaWEvU2hhbmdoYWkiLCJ1c2VyX25hbWUiOiJqb2huIiwic2NvcGUiOlsicmVhZCJdLCJleHAiOjE2NTg4OTE0NzgsImF1dGhvcml0aWVzIjpbInJlYWQiXSwianRpIjoiNThjZTg0MDUtNGVmYS00NDU2LWIyNmQtNzU4MDQyYjc3YWYyIiwiY2xpZW50X2lkIjoiY2xpZW50In0.DGkJK3B1h72oY4DNDKlI4buXtx4stFCQEsqcrpi7Li7LsgMmva0kbIhaqYWwZTJLDPhXPRkWJRA0btlgBuiYkwZ7fppyw2I5SN24zuFt7K23BbkIBbCDW1gSUwBYpznZm2Z8WTngeSiyK7LwqNI2BcqGwYJqoaJuTZKEy6WBgmZMJAQp8a4764yElPepfmTw7TofGQ7Fx0c4IK22WFSwfTto_kgA5FQ38-NYmEX9O2EP3J3yZbY5ZmJ0sXDHJc4ggPAQ7eA_Pf0rQDtOC6alGDAe1hukTsacpkjo9p3AVuuRFwl_lzeyuxmizi1sGsrmRz2ocXwIHBgtTv4FWCPydA","token_type":"bearer","refresh_token":"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJnZW5lcmF0ZUluWm9uZSI6IkFzaWEvU2hhbmdoYWkiLCJ1c2VyX25hbWUiOiJqb2huIiwic2NvcGUiOlsicmVhZCJdLCJhdGkiOiI1OGNlODQwNS00ZWZhLTQ0NTYtYjI2ZC03NTgwNDJiNzdhZjIiLCJleHAiOjE2NjE0NDAyNzgsImF1dGhvcml0aWVzIjpbInJlYWQiXSwianRpIjoiOWVlNGJlMjYtN2MxOS00OGFiLWFiNmYtYzg4ZTNhY2Y1NDRmIiwiY2xpZW50X2lkIjoiY2xpZW50In0.ELlgAYfEwDQQyTRI4F0BZVHLVMd4eQbT95OTQrXU1pt7t3OyFaHwNaaKeeoAIY_MpjoVib5k7DMqzSqQcA1wNcevgU2PB5PdGHCQo7h07kfF0oh4swPPoq97vfWq9HuTwAlWRqsE3bIpA1Y0IxdWGq_bkNHrXF46llDvunaRPmYK4ntnQsHuBlVdxVOoaUCyGTPw6YcXua4AurY-G1xHCsfV25i2sRay9V2z4wrvh106JGHwm8mDvIsGUjzbN5_nlg5hYgd95JYPxepWy5MuqosihrPTdQeZi7YfSY1h0855OSQpMmov9C490ncx6xtTz2sIWigCTYTJe4iZ7sK0lQ","expires_in":43199,"scope":"read","generateInZone":"Asia/Shanghai","jti":"58ce8405-4efa-4456-b26d-758042b77af2"} 解析access_token: 1.header:{              "alg": "RS256",              "typ": "JWT"          } 2.payload:{               "generateInZone": "Asia/Shanghai",               "user_name": "john",               "scope": ["read"],               "exp": 1658890328,               "authorities": ["read"],               "jti": "b66d7893-a45b-479d-8078-c2a34aed1699",               "client_id": "client"           } 观察解析后的jwt，我们可以可以看到增加了generateInZone字段

--- src/main/java/com/jjpeng/authorizationserver/config/AuthConfiguration.java
+++ src/main/java/com/jjpeng/authorizationserver/config/AuthConfiguration.java
@@ -1,5 +1,6 @@
 package com.jjpeng.authorizationserver.config;
 
+import com.jjpeng.authorizationserver.enhancer.CustomTokenEnhancer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,10 +11,15 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author JJPeng
@@ -38,9 +44,15 @@ public class AuthConfiguration extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+
+        List<TokenEnhancer> tokenEnhancers = Arrays.asList(new CustomTokenEnhancer(), jwtAccessTokenConverter());
+        tokenEnhancerChain.setTokenEnhancers(tokenEnhancers);
+
         endpoints.authenticationManager(authenticationManager)
                 .tokenStore(tokenStore())
-                .accessTokenConverter(jwtAccessTokenConverter());
+//                .accessTokenConverter(jwtAccessTokenConverter())
+                .tokenEnhancer(tokenEnhancerChain);
     }
 
     @Bean
